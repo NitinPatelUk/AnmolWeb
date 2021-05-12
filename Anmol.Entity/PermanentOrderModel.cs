3f561a3c608b756ffd729bc3face053f130322e1
aagamgopanisgit <agamgopanisgit@gmail.com>
2021-05-12 18:51:39 +0530

    PermenentOrder Model And Service File Changes

---
+++ Anmol.Entity/PermanentOrderModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Anmol.Entity
+{
+    public class PermanentOrderModel
+    {
+        public int Id { get; set; }
+
+        public int CustTD { get; set; }
+
+        public string DeliveryTime { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string LoggedinUserName { get; set; }
+
+    }
+}
