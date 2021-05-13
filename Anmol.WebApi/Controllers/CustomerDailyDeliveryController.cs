f8cb4b4ca94700238a214a6251e0bc8f3312055d
Parth <parth.d@sgit.in>
2021-05-13 11:34:06 +0530

    changes for customer daily delivery and hangfire packages.

---
+++ Anmol.WebApi/Controllers/CustomerDailyDeliveryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using _Anmol.Common;
+using _Anmol.Entity;
+using _Anmol.Service;
+
+namespace _Anmol.WebApi.Controllers
+{
+    [RoutePrefix("api")]
+    public class CustomerDailyDeliveryController : ApiController
+    {
+        private readonly CustomerDailyDeliveryService objService;        
+
+        public CustomerDailyDeliveryController()
+        {
+            objService = new CustomerDailyDeliveryService();            
+        }
+
+        [HttpPost]
+        [Route("CustomerDailyDelivery")]
+        public BaseApiResponse CustomerDailyDelivery()
+        {
+            var result = objService.CustomerDailyDelivery();
+            return result;
+        }
+    }
+}
