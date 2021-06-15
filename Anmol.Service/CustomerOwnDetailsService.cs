942aacdc92ae91dd11ed68e038addc4c66e11dab
aagamgopanisgit <agamgopanisgit@gmail.com>
2021-06-15 12:46:56 +0530

    My Profile Changes

---
+++ Anmol.Service/CustomerOwnDetailsService.cs
@@ -0,0 +1,35 @@
+﻿using _Anmol.Common;
+using _Anmol.Data.Repository;
+using _Anmol.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Anmol.Service
+{
+    public class CustomerOwnDetailsService
+    { 
+        public ApiResponse<CustomerOwnDetailsModel>GetCustomerOwnDetails(int? CustID)
+        {
+            ApiResponse<CustomerOwnDetailsModel> response = new ApiResponse<CustomerOwnDetailsModel>();
+            try
+            {
+                GenericRepository<CustomerOwnDetailsModel> objGenericRepository = new GenericRepository<CustomerOwnDetailsModel>();
+                var result = objGenericRepository.QuerySQL<CustomerOwnDetailsModel>("SP_GetCustomerOwnDetails", Utility.GetSQLParam("Name", SqlDbType.VarChar, (object)CustID ?? DBNull.Value));
+                response.Data = result.ToList();
+                response.Success = true;
+            }
+            catch (Exception ex)
+            {
+                response.Data = null;
+                response.Message.Add(ex.Message);
+                response.Success = false;
+            }
+            return response;
+        }
+    }
+
+}
